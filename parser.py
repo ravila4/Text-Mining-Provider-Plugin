f818716b800efdab36a1433d0d856978389c3571
Ricardo Avila <ravila@protonmail.com>
2020-08-04 13:40:52 -0700

    Add category keys for object, subject, and evedence

--- parser.py
+++ parser.py
@@ -16,24 +16,29 @@ def load_annotations(data_folder):
                     )
     edge_data = pd.read_csv(edge_file, sep="\t", quoting=csv.QUOTE_NONE)\
         .to_dict(orient='records')
-    node_data = pd.read_csv(node_file, sep="\t",
-                            usecols=["id", "publications", "sentence"])
+    node_data = pd.read_csv(node_file, sep="\t", usecols=[
+        "id", "category", "publications", "sentence"])
     results = {}
     for rec in edge_data:
         _id = rec["subject"]
         edge_label = rec["edge_label"].replace("biolink:", "")
         object_name = rec["object"].split(":")[0].lower()
         association_type = rec["association_type"].replace("biolink:", "")
+        subject_category = node_data.category[node_data.id == _id].squeeze()
+        object_category = node_data.category[
+                node_data.id == rec["object"]].squeeze()
         evidence = []
         if not pd.isna(rec["has_evidence"]):
             evidence_ids = rec["has_evidence"].split("|")
             for eid in evidence_ids:
-                docs = node_data[node_data["id"] == eid].squeeze()
-                evidence.append({"sentence": docs["sentence"],
-                                 "pmc": docs["publications"].replace(
-                                     "PMCID:", "")
-                                 })
+                docs = node_data[node_data.id == eid].squeeze()
+                evidence.append(
+                        {"category": docs["category"].replace("biolink:", ""),
+                         "sentence": docs["sentence"],
+                         "pmc": docs["publications"].replace("PMCID:", "")
+                         })
         data = {object_name: rec["object"],
+                "category": object_category,
                 "relation": rec["relation"],
                 "association_type": association_type,
                 "evidence": evidence}
